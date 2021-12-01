ef10437bde1c70befee6457e7ff8a0ee985b16ec
Juan Gabriel <juan192001-jg>
2021-11-30 19:50:29 -0500

    corecion en bases de datos

--- src/config.js
+++ src/config.js
@@ -5,12 +5,16 @@ import { config } from "dotenv";
 config();
 
 export default {
-  database: {
-    connectionLimit: 10,
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "faztpassword",
-    database: process.env.DATABASE_NAME || "dblinks",
-  },
-  port: process.env.PORT || 4000,
+    database: {
+        connectionLimit: 10,
+        // host: process.env.DATABASE_HOST || "localhost",
+        // user: process.env.DATABASE_USER || "root",
+        // password: process.env.DATABASE_PASSWORD || "",
+        // database: process.env.DATABASE_NAME || "dblinks",
+        host: "localhost",
+        user: "root",
+        passwd: "",
+        database: "dblinks"
+    },
+    port: process.env.PORT || 4000,
 };
